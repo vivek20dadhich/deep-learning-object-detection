2754be5c7768e07dedfe5069b8cc3b72cc936edd
vivek dadhich <ivivekdadhich@gmail.com>
2019-07-19 23:59:22 +0530

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -1,13 +1,23 @@
 import numpy as np
 import cv2
+import argparse
 
-net=cv2.dnn.readNetFromCaffe(r'C:\Users\ivive\Downloads\MobileNetSSD_deploy.prototxt',r'C:\Users\ivive\Downloads\MobileNetSSD_deploy.caffemodel')
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 	"sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 v=cv2.VideoCapture(0)
 while(True):
@@ -29,7 +39,7 @@ while(True):
      
             # filter out weak detections by ensuring the `confidence` is
             # greater than the minimum confidence
-            if confidence > 0.3:
+            if confidence > args["confidence"]:
                     # extract the index of the class label from the `detections`,
                     # then compute the (x, y)-coordinates of the bounding box for
                     # the object
